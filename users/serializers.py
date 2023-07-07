4522d8c9fd204d0351d9faeebb3a86205e3079a8
MatheusGualtieri <50122650+MatheusGualtieri@users.noreply.github.com>
2023-07-07 18:57:50 -0300

    feat: add read data to relation of books and users

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import User
+from books.models import Book
+from django.forms.models import model_to_dict
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,6 +16,8 @@ class UserSerializer(serializers.ModelSerializer):
             "password",
             "is_loan_blocked",
             "is_superuser",
+            "books_liked",
+            "books_follow",
         ]
         extra_kwargs = {
             "password": {"write_only": True},
@@ -38,6 +42,9 @@ class UserSerializer(serializers.ModelSerializer):
         ],
     )
 
+    books_liked = serializers.SerializerMethodField()
+    books_follow = serializers.SerializerMethodField()
+
     def create(self, validated_data: dict) -> User:
         if validated_data["is_superuser"]:
             return User.objects.create_superuser(**validated_data)
@@ -52,3 +59,23 @@ class UserSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
+
+    def get_books_liked(self, obj):
+        books = User.books_liked.through.objects.filter(user__pk=obj.id)
+        books_liked = []
+        for book in books:
+            item = Book.objects.get(id=book.book_id)
+            books_liked.append(
+                {"id": item.id, "name": item.name, "like_status": book.user_liked}
+            )
+        return books_liked
+
+    def get_books_follow(self, obj):
+        books = User.books_follow.through.objects.filter(user__pk=obj.id)
+        books_follow = []
+        for book in books:
+            item = Book.objects.get(id=book.book_id)
+            books_follow.append(
+                {"id": item.id, "name": item.name, "status": item.status}
+            )
+        return books_follow
